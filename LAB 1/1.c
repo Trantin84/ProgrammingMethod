440722067e8f290150f927169033f4f652325d18
Trantin84 <trantin84@gmail.com>
2020-10-30 11:16:24 +0700

    Create 1.c

---
+++ LAB 1/1.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                     Programming Methodlogy LAB Guidance
+                Code, Compile, Run and Debug C program online.
+For preferences only, it's not the complete solution. Some error might be placed for teaching purposes.
+
+*******************************************************************************/
+
+//Ex 1 Print your name, birthday and fone number.
+
+#include <stdio.h>  //For printf call
+
+int main()
+{
+    printf("Tran Trung Tin\n");
+    printf("20 Oct 1984\n");
+    printf("028-37755046\n");
+    return 0;
+}
